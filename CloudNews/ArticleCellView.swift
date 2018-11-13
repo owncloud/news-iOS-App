e4b08077b3af45e4eb96f2960423a607dfaa5486
Peter Hedlund <peter@peterandlinda.com>
2018-11-12 18:48:08 -0800

    More macOS work on article list.

---
+++ CloudNews/ArticleCellView.swift
@@ -0,0 +1,19 @@
+//
+//  ArticleCellView.swift
+//  CloudNews
+//
+//  Created by Peter Hedlund on 11/10/18.
+//  Copyright © 2018 Peter Hedlund. All rights reserved.
+//
+
+import Cocoa
+
+class ArticleCellView: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
